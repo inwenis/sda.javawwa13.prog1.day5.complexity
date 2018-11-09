f93690add8dbc462a1977f03dc5c4edf17400453
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-11-09 18:15:35 +0100

    measure times of finding elements

--- src/main/java/compare_time/Main.java
+++ src/main/java/compare_time/Main.java
@@ -2,9 +2,7 @@ package compare_time;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
         Integer[] array = new Integer[]{1, 2, 4, 100, 11, 21, 23};
-        boolean foundInArray = findInArray(array, 12);
 
         BstTree bst = new BstTree();
 
@@ -12,7 +10,17 @@ public class Main {
             bst.insert(i);
         }
 
-        boolean foundInBst = findInBst(bst ,12);
+        long before = System.nanoTime();
+        boolean foundInArray = findInArray(array ,12);
+        long after = System.nanoTime();
+        long elapsedNanoSeconds = after - before;
+        System.out.println("elapsed nanoseconds: " + elapsedNanoSeconds);
+
+        before = System.nanoTime();
+        boolean foundInBst = findInBst(bst, 12);
+        after = System.nanoTime();
+        elapsedNanoSeconds = after - before;
+        System.out.println("elapsed nanoseconds: " + elapsedNanoSeconds);
     }
 
     private static boolean findInBst(BstTree bst, int toBeFound) {
