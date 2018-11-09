1105c48178c508418a77ebe080c13cdb1d51d9af
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-11-09 18:35:32 +0100

    print average result

--- src/main/java/compare_time/Main.java
+++ src/main/java/compare_time/Main.java
@@ -1,5 +1,6 @@
 package compare_time;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Random;
 
@@ -7,16 +8,39 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int size = 100;
+        int size = 100000;
         Random random = new Random(new Date().getTime());
 
         Integer[] array = randomArray(size);
-
         Integer toBeFound = random.nextInt();
 
-        SingleTestResult result = measureTime(array, toBeFound);
-        System.out.println(String.format("%f", result.timeForArraySeconds));
-        System.out.println(String.format("%f", result.timeForBstSeconds));
+        int repeatRunForSingleDataSet = 10;
+
+        // multi tier jitting
+        measureTime(array, toBeFound);
+        measureTime(array, toBeFound);
+        measureTime(array, toBeFound);
+
+        SingleTestResult[] results = new SingleTestResult[repeatRunForSingleDataSet];
+        for (int i = 0; i < repeatRunForSingleDataSet; i++) {
+            SingleTestResult result = measureTime(array, toBeFound);
+            results[i] = result;
+            String formated = String.format("%f %f", result.timeForArraySeconds, result.timeForBstSeconds);
+            System.out.println(formated);
+        }
+
+        double averageTimeForArrayInSeconds = Arrays.stream(results)
+                .mapToDouble(x -> x.timeForArraySeconds)
+                .average()
+                .getAsDouble();
+
+        double averageTimeForBstInSeconds = Arrays.stream(results)
+                .mapToDouble(x -> x.timeForBstSeconds)
+                .average()
+                .getAsDouble();
+
+        String formated = String.format("%f %f", averageTimeForArrayInSeconds, averageTimeForBstInSeconds);
+        System.out.println(formated);
     }
 
     private static Integer[] randomArray(int size) {
