3aacae31cc54da873515e8311c25deb3516ec055
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-11-09 18:02:55 +0100

    initial commit

---
+++ src/main/java/compare_time/Main.java
@@ -0,0 +1,9 @@
+package compare_time;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        Integer[] array = 
+        findInArray()
+    }
+}
