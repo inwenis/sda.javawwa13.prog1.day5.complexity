8555b833bcd79609fa3de438b72a2dfd37b47ac2
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-11-17 13:40:42 +0100

    add tests measuring sorting times

---
+++ src/main/java/compare_time_sorting/SortingTestResultWithStats.java
@@ -0,0 +1,10 @@
+package compare_time_sorting;
+
+import compare_time.Statistics;
+
+public class SortingTestResultWithStats {
+    public double[] arrayTimesNoOutsiders;
+    public double[] bstTimesNoOutsiders;
+    public Statistics bubbleStatsNoOutsiders;
+    public Statistics javaSortStatsNoOutsiders;
+}
